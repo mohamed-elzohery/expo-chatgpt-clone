219f812e3b5df4c0249b6272b7fa19e7491aaeab
mohamed elzohery <m.zohery1998@gmail.com>
2024-08-15 17:21:34 +0300

    fix: restructure routes

--- app/auth/drawer/dalle.tsx
+++ app/auth/drawer/dalle.tsx
@@ -1,10 +1,35 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Stack } from "expo-router";
+import HeaderDropdown from "@/components/HeaderDropdown";
+import { defaultStyles } from "@/constants/Styles";
 
 const Dalle = () => {
+  // const [gptVersion, setGptVersion] = useState("");
+  // const onGptVersionChange = (version: string) => {
+  //   setGptVersion(version);
+  // };
   return (
-    <View>
-      <Text>Dalle</Text>
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <HeaderDropdown
+              title="Dall·E"
+              items={[
+                {
+                  key: "share",
+                  title: "Share GPT",
+                  icon: "square.and.arrow.up",
+                },
+                { key: "details", title: "See Details", icon: "info.circle" },
+                { key: "keep", title: "Keep in Sidebar", icon: "pin" },
+              ]}
+              onSelect={() => {}}
+            />
+          ),
+        }}
+      />
     </View>
   );
 };
